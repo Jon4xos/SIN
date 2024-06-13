616801056485e8c12aafb79b232ed6608594208f
Jonas Rauchardt <jonas.rauchardt@web.de>
2024-06-13 22:52:24 +0200

    fixed hash data

--- src/HashVerification.java
+++ src/HashVerification.java
@@ -1,13 +1,16 @@
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
+import java.security.Security;
 import java.util.Arrays;
 
 public class HashVerification {
     public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
         String textFilePath = "src/plaintext.txt";
-        String hashFilePath = "md5hash.txt";
-        String algorithm = "MD5";
+        String hashFilePath = "ripemd160hash.txt";
+        String algorithm = "RIPEMD160";
 
         byte[] textBytes = Files.readAllBytes(Paths.get(textFilePath));
         byte[] storedHash = Files.readAllBytes(Paths.get(hashFilePath));
